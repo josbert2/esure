61b874a4f86d286a5474afd2984d85444a42e158
jos <joheandroid@gmail.com>
2021-02-04 11:46:22 -0300

    Fix

--- index.php
+++ index.php
@@ -462,11 +462,10 @@ function consultarPropiedades(obj){
 			async: false,
 			url:  urlPath + '/modules_/imno/consulta.php',
 			type: 'post',
-			data: obj
-		}).done(
-			function (resp) {
+			data: obj,
+			success: function (resp) {
 				resp = resp.trim();
-				
+				console.log(resp)
 				if(eval(resp)){
 					var objData = eval(resp);
 					cargarNormal(objData);
@@ -481,8 +480,8 @@ function consultarPropiedades(obj){
 					prevArrow:'<div class="slick-d slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M14.71 15.88L10.83 12l3.88-3.88c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L8.71 11.3c-.39.39-.39 1.02 0 1.41l4.59 4.59c.39.39 1.02.39 1.41 0 .38-.39.39-1.03 0-1.42z"/></svg></div>',
                     nextArrow:'<div class="slick-d slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9.29 15.88L13.17 12 9.29 8.12c-.39-.39-.39-1.02 0-1.41.39-.39 1.02-.39 1.41 0l4.59 4.59c.39.39.39 1.02 0 1.41L10.7 17.3c-.39.39-1.02.39-1.41 0-.38-.39-.39-1.03 0-1.42z"/></svg></div>'
 				});
-			}		
-		);		
+			}
+		})		
 	 },1000);
 	
 };
