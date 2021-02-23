667a63590ab560a9dda4cc70e529521910147e0d
jos <joheandroid@gmail.com>
2021-02-23 09:47:29 -0300

    Fix

--- index.php
+++ index.php
@@ -319,7 +319,14 @@ function buscarPropiedades(){
 	
 		}
 	);
-	
+	$('.slick-img').slick({
+		dots: true,
+		nav: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow:'<div class="slick-d slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M14.71 15.88L10.83 12l3.88-3.88c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L8.71 11.3c-.39.39-.39 1.02 0 1.41l4.59 4.59c.39.39 1.02.39 1.41 0 .38-.39.39-1.03 0-1.42z"/></svg></div>',
+		nextArrow:'<div class="slick-d slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9.29 15.88L13.17 12 9.29 8.12c-.39-.39-.39-1.02 0-1.41.39-.39 1.02-.39 1.41 0l4.59 4.59c.39.39.39 1.02 0 1.41L10.7 17.3c-.39.39-1.02.39-1.41 0-.38-.39-.39-1.03 0-1.42z"/></svg></div>'
+	});
 };
 
 
